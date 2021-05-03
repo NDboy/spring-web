196c11b33ae9de74f720153e4589d707ded9920d
NDboy <endre.karsai@gmail.com>
2021-05-03 11:15:49 +0200

    Initial commit

---
+++ src/main/java/spring/web/WebAppInitializer.java
@@ -0,0 +1,22 @@
+package spring.web;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+import spring.web.controller.WebConfig;
+
+public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return null/*new Class[]{}*/;
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{WebConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] {"/"};
+    }
+}
---
+++ src/main/java/spring/web/controller/HelloController.java
@@ -0,0 +1,19 @@
+package spring.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+//@RequestMapping("/hello")
+//@ResponseBody
+public class HelloController {
+
+
+    @RequestMapping("/hello")
+    @ResponseBody
+    public String hello() {
+        System.out.println("Hello SpringMVC-----------------------------------------------");
+        return "Hello SpringMVC";
+    }
+}
